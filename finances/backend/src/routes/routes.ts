c567ff8400d7d7d8de82a2b5212b026535f882c2
DiogoMEng <diogoeng19@gmail.com>
2025-03-11 17:57:54 -0300

    :bricks: ci: criação de migrations finalizada

--- finances/backend/src/routes/routes.ts
+++ finances/backend/src/routes/routes.ts
@@ -1,29 +1,5 @@
 import express from "express";
-// import {
-//   account,
-//   accountCreditExpenses,
-//   accountExpenses,
-// } from "../controllers/accountInformation.controller";
-// import {
-//   credit,
-//   deleteExpenseFromAccount,
-//   expense,
-//   receipt,
-// } from "../controllers/accountMovement.controller";
 
 const router = express.Router();
-
-// // VIEWS
-// router.get("/balances", account);
-// router.get("/balances/expenses", accountExpenses);
-// router.get("/balances/credit", accountCreditExpenses);
-
-// // ENTRIES
-// router.post("/balance", receipt);
-// router.post("/balance/expense", expense);
-// router.post("/balance/credit", credit);
-
-// // EXITS
-// router.delete("/balance/expense/:idExpense", deleteExpenseFromAccount);
 
 export default router;
