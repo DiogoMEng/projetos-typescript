b2ffb9d725a814f98c1d74f065ae9a2653650823
Diogo Mello <diogoeng19@gmail.com>
2025-05-26 11:10:20 -0300

    :sparkles: feat: Implementação da Funcionalidade de exclusão na API

--- finances/backend/src/controllers/income.controller.ts
+++ finances/backend/src/controllers/income.controller.ts
@@ -22,7 +22,7 @@ class IncomeController {
 
       const { ...entry } = newEntry.get({ plain: true });
 
-      return res.status(200).json({ message: "Entry created successfully" });
+      return res.status(200).json({ message: "Novo valor de entrada registrado com sucesso" });
     } catch (error) {
       res.status(500).json({ message: "Internal server error" });
     }
@@ -48,12 +48,46 @@ class IncomeController {
   async delete(req: Request, res: Response, next: NextFunction) {
 
     try {
+      const { userId } = req.user as AuthenticatedUser;
       const { id } = req.params;
+
+      const user = await this.model.findAll({ where: { userId } });
+
+      if (!user) {
+        return res.status(404).json({ message: "Usuário não registrado. Realize o seu cadastro" });
+      }
 
       const entry = await this.model.findByPk(id);
 
       if (!entry) {
-        return res.status(404).json({ message: "Registro inválido" });
+        return res.status(404).json({ message: "Ocorreu um erro ao tentar deletar o registro" });
+      }
+
+      await entry.destroy();
+
+      return res.status(200).json({ message: "Registro de entrada deletado" });
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
+
+  }
+
+  async update(req: Request, res: Response, next: NextFunction) {
+
+    try {
+      const { userId } = req.user as AuthenticatedUser;
+      const { id } = req.params;
+
+      const user = await this.model.findAll({ where: { userId } });
+
+      if (!user) {
+        return res.status(404).json({ message: "Usuário não registrado. Realize o seu cadastro" });
+      }
+
+      const entry = await this.model.findByPk(id);
+
+      if (!entry) {
+        return res.status(404).json({ message: "Ocorreu um erro ao tentar deletar o registro" });
       }
 
       await entry.destroy();
